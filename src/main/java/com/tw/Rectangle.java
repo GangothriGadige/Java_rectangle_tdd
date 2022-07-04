c4e0deccca96be15ec54e57b129d701d6686adc5
Gangothri <gangothrigadige985@gmail.com>
2022-07-05 00:03:08 +0530

    To add rectangle class which calculates area and perimeter of rectangle

--- src/main/java/com/tw/Rectangle.java
+++ src/main/java/com/tw/Rectangle.java
@@ -1,4 +1,20 @@
 package com.tw;
 
 public class Rectangle {
+    private final int length;
+    private final int breadth;
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int area() {
+        return length * breadth;
+    }
+
+    public int perimeter() {
+        return 2 * (length + breadth);
+    }
 }
+
--- src/test/java/com/tw/RectangleTest.java
+++ src/test/java/com/tw/RectangleTest.java
@@ -1,4 +1,43 @@
 package com.tw;
 
-public class RectangleTest {
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class RectangleTest {
+
+    @Test
+    void should_have_an_area_of_one_if_length_one_and_breadth_one() {
+        Rectangle rectangle = new Rectangle(1, 1);
+
+        assertThat(rectangle.area(), is(equalTo(1)));
+    }
+
+    @Test
+    void should_have_an_area_of_two_if_length_two_and_breadth_one() {
+        Rectangle rectangle = new Rectangle(2, 1);
+
+        assertThat(rectangle.area(), is(equalTo(2)));
+    }
+
+    @Test
+    void should_have_a_perimeter_of_four_if_length_one_and_breadth_one() {
+        Rectangle rectangle = new Rectangle(1, 1);
+
+        assertThat(rectangle.perimeter(), is(equalTo(4)));
+    }
+
+    @Test
+    void should_have_a_perimeter_of_six_if_length_two_and_breadth_one() {
+        Rectangle rectangle = new Rectangle(2, 1);
+
+        assertThat(rectangle.perimeter(), is(equalTo(6)));
+    }
+
 }
+
+
+
+
